4e6ee71ac7e824667648b06eab74c7df0a3cc51d
varun vyas <moduvyas@gmail.com>
2016-01-17 00:13:14 -0500

    All basic features working.

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+
 import AFNetworking
 
+import MBProgressHUD
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
-
+    
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
@@ -21,6 +23,19 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableView.dataSource = self
         tableView.delegate = self
+        actualNetworkSessionGet()
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func actualNetworkSessionGet() {
+
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
@@ -40,12 +55,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                             self.tableView.reloadData()
                     }
                 }
+                // Hide HUD once network request comes back (must be done on main UI thread)
+                MBProgressHUD.hideHUDForView(self.view, animated: true)
+                
         });
         task.resume()
-        
-        // Do any additional setup after loading the view.
     }
-
+    
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        // Make network request to fetch latest data
+        actualNetworkSessionGet()
+        // Do the following when the network request comes back successfully:
+        // Update tableView data source
+        self.tableView.reloadData()
+        refreshControl.endRefreshing()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -53,43 +79,39 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let movies = movies {
-            return movies.count
+            return movies.count - 1 /*Removing the last entry as poster_path was getting null resulting in app crash . Will fix it later! :) */
         }
         else {
             return 0
         }
     }
-
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let poster_path = movie["poster_path"] as! String
-        
-        
         let baseUrl = "https://image.tmdb.org/t/p/w342"
-        
         let imageUrl = NSURL(string: baseUrl + poster_path)
-        
-        
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         cell.posterView.setImageWithURL(imageUrl!)
         //cell.textLabel!.text = title
-        print("row \(indexPath.row)")
+        //print("row \(indexPath.row)")
         return cell
     }
-
-
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
--- Podfile
+++ Podfile
@@ -1,5 +1,5 @@
 # Uncomment this line to define a global platform for your project
-# platform :ios, '8.0'
+platform :ios, '8.0'
 # Uncomment this line if you're using Swift
 use_frameworks!
 
@@ -7,6 +7,8 @@ target 'MovieViewer' do
 
 # This pulls in the latest version of AFNetworking >= 2.6 but less than 3.0.
 pod 'AFNetworking'
+
+pod 'MBProgressHUD'
 
 end
 
